a45f0c0d3ea6a638d4b370c9657b6e06b53aa8b2
ben.camilleri <ben.camilleri@gig.com>
2019-11-11 14:03:04 +0100

    syntax cleanup

--- weathercomparer/tests/openweather_requester_test.go
+++ weathercomparer/tests/openweather_requester_test.go
@@ -21,7 +21,7 @@ func TestWeatherRequest_OpenWeather_Success200(t *testing.T) {
 		res.Header().Set("Content-Type", "application/json")
 		n, err :=io.WriteString(res, "{\"coord\":{\"lon\":12.48,\"lat\":41.89},\"weather\":[{\"id\":802,\"main\":\"Clouds\",\"description\":\"scattered clouds\",\"icon\":\"03n\"}],\"base\":\"stations\",\"main\":{\"temp\":61.05,\"pressure\":1005,\"humidity\":87,\"temp_min\":54,\"temp_max\":66.99},\"visibility\":10000,\"wind\":{\"speed\":4.7,\"deg\":190},\"clouds\":{\"all\":40},\"dt\":1572985001,\"sys\":{\"type\":1,\"id\":6792,\"country\":\"IT\",\"sunrise\":1572932808,\"sunset\":1572969629},\"timezone\":3600,\"id\":6539761,\"name\":\"Rome\",\"cod\":200}")
 		if err != nil {
-			fmt.Println(n, err)
+			t.Logf(n, err)
 		}
 	}))
 
